fb75245aae08f09504691fee74977ec07b6c2c72
Mop321 <aweds200@gmail.com>
2025-07-20 08:34:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Rec/", // ✅ MUST match repo name
+  base: '/lo/',
   plugins: [react()],
-});
+})
